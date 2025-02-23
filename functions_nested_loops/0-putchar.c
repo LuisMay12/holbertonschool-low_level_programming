a502c7304343b8cf43357e383a56bc1632634827
Luis May <a20214976@alumons.uady.mx>
2025-02-23 22:18:28 +0000

    put char solution

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line
+ *
+ * Description: This program prints "_putchar" using
+ * the `_putchar`
+ * function. No other functions like `printf` or `puts` are used.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
