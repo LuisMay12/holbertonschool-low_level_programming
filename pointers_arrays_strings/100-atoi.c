b6051b90cfbba1dda68563f1f57525352f204dcc
Luis May <a20214976@alumnos.uady.mx>
2025-03-02 15:41:56 +0000

    fixing

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,25 +11,20 @@ int _atoi(char *s)
 int i = 0;
 int sign = 1;
 unsigned int result = 0;
+int found_number = 0;
 
-while (s[i] == ' ' || s[i] == '+' || s[i] == '-')
+while (s[i] != '\0')
 {
 if (s[i] == '-')
 sign *= -1;
-i++;
-}
-
-while (s[i] >= '0' && s[i] <= '9')
+else if (s[i] >= '0' && s[i] <= '9')
 {
-if (result > 2147483647 / 10 || (result == 2147483647 / 10 && s[i] - '0' > 7))
-{
-if (sign == 1)
-return (2147483647);
-else
-return (-2147483648);
-}
-
 result = result * 10 + (s[i] - '0');
+found_number = 1;
+}
+else if (found_number)
+break;
+
 i++;
 }
 
