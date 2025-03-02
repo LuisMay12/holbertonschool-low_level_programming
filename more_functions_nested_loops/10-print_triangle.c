0d610728d6fad0e222a587873c389a5d4eb26c6b
Luis May <a20214976@alumnos.uady.mx>
2025-03-02 01:30:39 +0000

    10-triangles solution

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a right-aligned triangle using '#' characters.
+ * @size: The height of the triangle.
+ */
+void print_triangle(int size)
+{
+int i, j, spaces;
+
+if (size > 0)
+{
+for (i = 1; i <= size; i++)
+{
+for (spaces = size - i; spaces > 0; spaces--)
+_putchar(' ');
+
+for (j = 0; j < i; j++)
+_putchar('#');
+
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
