1b39d56250c8fa898feff501c4fa7242799de2fd
Luis May <a20214976@alumnos.uady.mx>
2025-03-02 14:53:19 +0000

    fix3

--- pointers_arrays_strings/6-main.c
+++ pointers_arrays_strings/6-main.c
@@ -9,7 +9,13 @@ int main(void)
 {
     char *str;
 
-    str = "Lorem ipsum...";
+    str = "Lorem ipsum dolor sit amet, consectetur "
+      "Sed do eiusmod tempor incididunt ut labore et dolore "
+      "Ut enim ad minim veniam, quis nostrud exercitation "
+      "nisi ut aliquip ex ea commodo consequat. Duis aute "
+      "reprehenderit in voluptate velit esse cillum dolore "
+      "Excepteur sint occaecat cupidatat non proident, sunt "
+      "deserunt mollit anim id est laborum.";
     puts2(str);
     return (0);
 }
