dbe11a22ecb521c100ca8753a68fa32ec2ebebd0
Luis May <a20214976@alumnos.uady.mx>
2025-03-02 15:14:42 +0000

    atoi sol

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: Pointer to the string
+ *
+ * Return: The converted integer value
+ */
+int _atoi(char *s)
+{
+int i = 0;
+int sign = 1;
+int result = 0;
+int started = 0;
+
+while (s[i] != '\0')
+{
+if (s[i] == '-')
+sign *= -1;
+else if (s[i] >= '0' && s[i] <= '9')
+{
+result = result * 10 + (s[i] - '0');
+started = 1;
+}
+else if (started)
+break;
+
+i++;
+}
+
+return (result * sign);
+}
