83f8106a278e16aaff3ddac889f694a0bd292709
Luis May <a20214976@alumnos.uady.mx>
2025-03-08 13:02:43 +0000

    strchr sol

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to the string
+ * @c: character to locate
+ *
+ * Return: pointer to the first
+ * occurrence of c in s, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+while (*s)
+{
+if (*s == c)
+return (s);
+s++;
+}
+if (c == '\0')
+return (s);
+
+return (0);
+}
