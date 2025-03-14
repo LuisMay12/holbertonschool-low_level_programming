c76aa5e8fb414e6575a671a3160cb0bc61a76e00
Luis May <a20214976@alumnos.uady.mx>
2025-03-14 14:28:00 +0000

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a new type struct dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: A structure to store information
+ * about a dog.
+ */
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+#endif /* DOG_H */
